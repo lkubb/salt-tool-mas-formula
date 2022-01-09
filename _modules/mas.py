7fd002be9a8cae238aac5cad7399579db984b999
jeanluc <lkubb@protonmail.com>
2022-01-09 12:15:14 +0100

    Fixes mas.remove (sends to trash in python)

--- _modules/mas.py
+++ _modules/mas.py
@@ -1,10 +1,6 @@
 from salt.exceptions import CommandExecutionError
-# import logging
-import json
-
 import salt.utils.platform
 
-# log = logging.getLogger(__name__)
 __virtualname__ = "mas"
 
 
@@ -43,12 +39,49 @@ def install(name, user=None):
 
 
 def remove(name, user=None):
-    e = _which(user)
+    # https://github.com/mas-cli/mas/issues/313
+    # requires root permissions to move to trash, but when running as root,
+    # does not find the installed app. since mas only moves to trash, replicate
+    # that in python
+    from ctypes import cdll, byref, Structure, c_char, c_char_p
+    from ctypes.util import find_library
 
-    if not _is_id(name) and not (name := _get_local_id(name)):
-        raise CommandExecutionError("Could not find installation of '{}'.".format(name))
+    Foundation = cdll.LoadLibrary(find_library("Foundation"))
+    CoreServices = cdll.LoadLibrary(find_library("CoreServices"))
+    Foundation.GetMacOSStatusCommentString.restype = c_char_p
 
-    return not __salt__['cmd.retcode']("{} uninstall {}".format(e, name), runas=user)
+    class Ref(Structure):
+        _fields_ = [("hidden", c_char * 80)]
+
+    def check(res):
+        if (res):
+            msg = Foundation.GetMacOSStatusCommentString(res).decode("utf-8")
+            raise CommandExecutionError(msg)
+
+    def trash(path):
+        if not isinstance(path, bytes):
+            path = path.encode('utf-8')
+        f = Ref()
+        res = CoreServices.FSPathMakeRefWithOptions(path, 0x01, byref(f), None)
+        check(res)
+        res = CoreServices.FSMoveObjectToTrashSync(byref(f), None, 0)
+        check(res)
+
+    # this part would fail in mas since it was running as root
+    appname = _get_local_name(name, user)
+
+    if not appname:
+        raise CommandExecutionError("Could not find installed application '{}'.".format(name))
+
+    # that is an assumption, not sure if always true
+    path = '/Applications/{}.app'.format(appname)
+
+    if not __salt__['file.find'](path):
+        raise CommandExecutionError("Could not find '{}.app' in /Applications".format(appname))
+
+    __salt__['log.info']("Sending {} to trash.".format(path))
+    trash(path)
+    return True
 
 
 def upgrade(name, user=None):
@@ -63,6 +96,7 @@ def upgrade(name, user=None):
 def _list_installed(user=None):
     e = _which(user)
     ls = _parse_list(__salt__['cmd.run_stdout']("{} list".format(e), raise_err=True, runas=user))
+    __salt__['log.info']("mas list of installed apps: {}".format(ls))
     return {x[0]: x[1] for x in ls}
 
 
@@ -88,9 +122,20 @@ def _find_id(name, user=None):
 def _get_local_id(name, user=None):
     if _is_id(name):
         return name
-    for i, appname in _list_installed().items():
+    for i, appname in _list_installed(user).items():
         if appname == name:
+            __salt__['log.info']("mas found id of installed app '{}': {}".format(name, i))
             return i
+    return False
+
+
+def _get_local_name(appid, user=None):
+    if not _is_id(appid):
+        return appid
+    for i, appname in _list_installed(user).items():
+        if i == str(appid):
+            __salt__['log.info']("mas found name of installed app '{}': {}".format(appid, appname))
+            return appname
     return False
 
 
